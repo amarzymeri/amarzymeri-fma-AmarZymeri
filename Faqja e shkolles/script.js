e4f8e2cf015e167a98b1c4db5d5ddce7bf111ea6
amarzymeri <147183434+amarzymeri@users.noreply.github.com>
2024-10-20 13:18:47 +0200

    my school

--- Faqja e shkolles/script.js
+++ Faqja e shkolles/script.js
@@ -1,12 +1,9 @@
-function showInfo(element, subject, teacher) {
-    // Check if the subject already has the info showing
-    if (element.classList.contains('active')) {
-        // If active, remove the info and the active state
-        element.innerHTML = subject;
-        element.classList.remove('active');
+function showInfo(obj, subject, teacher) {
+    if (obj.classList.contains('active')) {
+        obj.innerHTML = subject;
+        obj.classList.remove('active');
     } else {
-        // If not active, show the teacher info inside the subject box
-        element.innerHTML = `Name: ${teacher}<br>Subject: ${subject}`;
-        element.classList.add('active');
+        obj.innerHTML = `Name: ${teacher}<br>Subject: ${subject}`;
+        obj.classList.add('active');
     }
 }
