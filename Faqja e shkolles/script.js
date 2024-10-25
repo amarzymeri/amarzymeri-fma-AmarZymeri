2bff4b39b3b80d8f261a05787678a08327c0c004
amarzymeri <147183434+amarzymeri@users.noreply.github.com>
2024-10-25 16:15:48 +0200

    hi

--- Faqja e shkolles/script.js
+++ Faqja e shkolles/script.js
@@ -1,3 +1,39 @@
+let subjects = [
+    {
+        Lënda: 'Matematikë',
+        teacher: 'Makfire'
+    },
+    {
+        Lënda: 'Fizikë',
+        teacher: 'Ms. e Fizikës'
+    },
+    {
+        Lënda: 'Histori',
+        teacher: 'Arsim'
+    },
+    {
+        Lënda: 'Anglisht',
+        teacher: 'Shukrije'
+    },
+    {
+        Lënda: 'Art',
+        teacher: 'Ragip'
+    },
+    {
+        Lënda: 'Edukatë Fizike',
+        teacher: 'Burbuqe'
+    }
+]
+
+const subjectBox = document.querySelector(".subject-box");
+
+for (let i = 0; i < subjects.length; i++) {
+    subjectBox.innerHTML += `
+        <div class="subject" onclick="showInfo(this, '${subjects[i].Lënda}', '${subjects[i].teacher}')">
+            ${subjects[i].Lënda}
+        </div>`;
+}
+
 function showInfo(obj, subject, teacher) {
     if (obj.classList.contains('active')) {
         obj.innerHTML = subject;
